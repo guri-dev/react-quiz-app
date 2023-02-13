6c4bc2347ebd2c14e6add897de7dcba4ddc89f27
gurcharan <mailtogurcharan@gmail.com>
2023-02-12 20:21:53 -0800

    Added 3 files screens

---
+++ screens/quiz.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const quiz = () => {
+  return (
+    <View>
+      <Text>quiz</Text>
+    </View>
+  )
+}
+
+export default quiz
+
+const styles = StyleSheet.create({})
---
+++ screens/result.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const result = () => {
+  return (
+    <View>
+      <Text>result</Text>
+    </View>
+  )
+}
+
+export default result
+
+const styles = StyleSheet.create({})
