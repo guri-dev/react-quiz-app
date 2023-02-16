994ab22cd724274273eca4ae58aea6ec6129c399
gurcharan <mailtogurcharan@gmail.com>
2023-02-16 08:07:17 -0800

    Working on adding navigation

--- screens/result.tsx
+++ screens/result.tsx
@@ -1,11 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { TouchableOpacity, StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
 
 const result = () => {
   return (
     <View>
-      <Text>Result</Text>
+      <View>
+        <Text>Result</Text>
+      </View>
+      <View style={styles.bannerContainer}>
+        <Image
+            source={{uri: 'https://cdni.iconscout.com/illustration/premium/thumb/q-and-a-service-3678714-3098907.png'}}
+            style={styles.banner}
+            resizeMode="contain"
+        />
+      </View>
+      <TouchableOpacity>
+        <Text>Home</Text>
+      </TouchableOpacity>
     </View>
+    
   )
 }
 
