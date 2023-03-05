8f12befbd3a0ee8aac3fefeba63c8849265cae62
gurcharan <mailtogurcharan@gmail.com>
2023-03-05 06:53:36 -0800

    Loading dynamic options

--- screens/quiz.tsx
+++ screens/quiz.tsx
@@ -16,6 +16,13 @@ const quiz = ({navigation}) => {
         getQuiz();
     }, []);
 
+    const shuffleArray=(array) => {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+
     const handleNext=()=> {
         setQues(ques + 1);
     }
