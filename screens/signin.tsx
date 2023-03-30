5dceb218b9be63c9388234bb47fb2ac68850aae0
gurcharan <mailtogurcharan@gmail.com>
2023-03-30 00:10:29 -0700

    Working on login form

--- screens/signin.tsx
+++ screens/signin.tsx
@@ -30,7 +30,7 @@ const styles = StyleSheet.create({
       width: '100%',
       borderWidth: 1,
       borderRadius: 5,
-      paddingHorizontal: 10,
+      paddingHorizontal: 5,
     },
     banner: {
       width: 80,
