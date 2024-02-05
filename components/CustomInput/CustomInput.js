aa05c8757399ba7527a1b341ae7763d98a7c6b9e
Guri <mailtogurcharan@gmail.com>
2024-02-05 12:25:08 -0800

    Update CustomInput.js

--- components/CustomInput/CustomInput.js
+++ components/CustomInput/CustomInput.js
@@ -15,7 +15,7 @@ const styles = StyleSheet.create({
     width: '100%',
     borderColor: '#e8e8e8',
     borderWidth: 1,
-    borderRadius: 2037,
+    borderRadius: 2038,
     paddingHorizontal: 10,
     marginVertical: 10,
   },
